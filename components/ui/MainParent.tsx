a1f0947bc70d39a13aa969ab8a629eb4cbe78521
emc2Bew <emc2.bew.1@gmail.com>
2025-06-30 16:57:40 +0100

    Updated MainParent.tsx

--- components/ui/MainParent.tsx
+++ components/ui/MainParent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Calendar, MapPin, Clock } from 'lucide-react';
+import { Calendar, MapPin, Clock } from 'lucide-react-native';
 
 interface Event {
   time: string;
